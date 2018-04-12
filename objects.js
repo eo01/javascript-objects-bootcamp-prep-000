68d7d20433c7d6494f8e9b45505e0cda06507ada
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-12 18:44:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {band: "song1"};
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {["artistName"]: songTitle})
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
