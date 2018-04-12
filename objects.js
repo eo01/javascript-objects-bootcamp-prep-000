75e274fd2606e3ee732b8e8ef1b9b1f10450f987
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-12 18:54:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist
 }
-function removeFromPlaylist(){
+function removeFromPlaylist(playlist, artistName){
   
 }
